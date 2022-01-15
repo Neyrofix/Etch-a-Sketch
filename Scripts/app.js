05b5d69ff9631c1d647fe45d111ea7dd85d2ad6b
Neurofix <neyrofix@gmail.com>
2022-01-15 16:09:11 +0500

    add table and buttons

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,14 @@
-alert('hi');
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 16; i++) {
+    const table = document.createElement('div');
+    table.classList.add('row');
+    container.appendChild(table);
+
+    for (let j = 0; j < 16; j++) {
+        const row = document.createElement('div');
+        row.classList.add('col');
+        row.textContent = 'B';
+        table.appendChild(row);
+    }
+}
